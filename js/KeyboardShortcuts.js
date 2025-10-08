fbf2f82f4a0365882011bff9c86e9a3665ae4290
yuri <shamuray22@gmail.com>
2025-10-08 11:31:56 -0300

    adacionado mais atalhos

--- js/KeyboardShortcuts.js
+++ js/KeyboardShortcuts.js
@@ -1,10 +1,18 @@
 document.addEventListener('keydown', function(event) {
+    event.preventDefault();
     if(event.key === 'z' && (event.ctrlKey || event.metaKey)){
         undo();
-        console.log('w');
-        
     } else if(event.key === 'y' && (event.ctrlKey || event.metaKey)){
         redo();
+    }else if (event.key === 's' && (event.ctrlKey || event.metaKey)){
+        saveCanvas();
+    }else if (event.key === 'c' && (event.ctrlKey || event.metaKey)){
+        clearCanvas();
+    }else if (event.key === 'e'){
+        set_current_tool('eraser');
+    } else if (event.key === 'q'){
+        set_current_tool('pen');
     }
+
     
 });
