9bcc0856422710256b893ba57ed2866133b29f5d
yuri <shamuray22@gmail.com>
2025-11-02 15:43:22 -0300

    sistema de themas adicionado

--- www/globals/globalDebug.js
+++ www/globals/globalDebug.js
@@ -1,5 +1,19 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const themesave = localStorage.getItem('theme');
+    console.log(themesave);
+
     console.log(localStorage);
-    
+
+    setTheme(themesave);
+
 })
+
+////////////// THEME SETTING ///////////////
+function setTheme(theme) {
+    document.documentElement.classList.remove('white-theme', 'pink-theme', 'blue-theme');
+    if (theme === 'white') document.documentElement.classList.add('white-theme');
+    else if (theme === 'pink') document.documentElement.classList.add('pink-theme');
+    else if (theme === 'blue') document.documentElement.classList.add('blue-theme');
+    localStorage.setItem('theme', theme)
+}
